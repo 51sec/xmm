3e823d8247796a41fba5ad62c4047492b337c510
51sec <johnyan.ca@gmail.com>
2025-07-17 16:04:44 -0400

    Update index.php

--- index.php
+++ index.php
@@ -53,7 +53,7 @@ body {background:#000;}
 @media screen and (min-width: 769px) {
 .app{left:50%;transform: translateX(-50%);
     width: 30%;
-    height: 100vh;
+    height: 90vh;
     position: absolute; 
     top:0px; 
     bottom: 0px; 
@@ -77,7 +77,8 @@ body {background:#000;}
 @media screen and (max-width: 768px) {
 .app{
     width: 100%; 
-    height:100%; 
+    height:auto; 
+    max-height: 90vh; 
     position: absolute; 
     top:0px; 
     right:0px; 
